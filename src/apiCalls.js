0c80fda5075a1738447efcbf6992afe3f3f4d40a
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-04-26 09:58:22 -0600

    Add post request and change format of fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,21 @@
-export const getUrls = () => {
-  return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+const fetchData = {
+	getUrls() {
+		return fetch('http://localhost:3001/api/v1/urls').then((response) =>
+			response.json()
+		)
+	},
+	postUrl(newUrl) {
+		return fetch('http://localhost:3001/api/v1/urls', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				long_url: newUrl.long_url,
+				title: newUrl.title,
+			}),
+		})
+	},
 }
+
+export default fetchData
