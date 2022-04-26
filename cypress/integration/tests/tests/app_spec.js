f152185b38973cd2e400f05e7ccbe8726227a571
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-04-26 11:11:48 -0600

    Break test up to be more atomic

--- cypress/integration/tests/tests/app_spec.js
+++ cypress/integration/tests/tests/app_spec.js
@@ -16,9 +16,12 @@ describe('Url-Shortener', () => {
 			.contains('Awesome photo')
 	})
 	it('When a user visits the page, they should be able to view the Form with the proper inputs', () => {
+		cy.intercept('http://localhost:3001/api/v1/urls', { fixture: 'urls.json' })
+		cy.visit('http://localhost:3000/')
 		cy.get('.form').get('#title').should('be.visible')
 		cy.get('.form').get('#longURL').should('be.visible')
 	})
+
 	it('When a user fills out the form, the information should be reflected in the input fields', () => {
 		cy.get('.form').get('#title').type('coolest image of a cow')
 		cy.get('.form')
@@ -27,10 +30,11 @@ describe('Url-Shortener', () => {
 				'https://i.pinimg.com/originals/5a/69/ba/5a69baacae4ad087da35b2b594b06edf.jpg'
 			)
 	})
-	it.only('should show a new shortened URL after a user fills out and submits the form', () => {
-		cy.visit('http://localhost:3000/')
-
+	it('should show a new shortened URL after a user fills out and submits the form', () => {
 		cy.get('.form').get('#submit').click()
+	})
+
+	it('should show the new card after submission with a shortened url', () => {
 		cy.get('.App')
 			.get('.url')
 			.get('.url-container')
