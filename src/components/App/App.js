9c63f6d72db81d053681283247c832b9349516fa
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-04-26 09:58:42 -0600

    Fix componentDidMount fn

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,52 @@
-import React, { Component } from 'react';
-import './App.css';
-import { getUrls } from '../../apiCalls';
-import UrlContainer from '../UrlContainer/UrlContainer';
-import UrlForm from '../UrlForm/UrlForm';
+import React, { Component } from 'react'
+import './App.css'
+import fetchData from '../../apiCalls'
+import UrlContainer from '../UrlContainer/UrlContainer'
+import UrlForm from '../UrlForm/UrlForm'
 
 export class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      urls: []
-    }
-  }
+	constructor() {
+		super()
+		this.state = {
+			urls: [],
+			error: '',
+		}
+	}
 
-  componentDidMount() {
-  }
-
-  render() {
-    return (
-      <main className="App">
-        <header>
-          <h1>URL Shortener</h1>
-          <UrlForm />
-        </header>
-
-        <UrlContainer urls={this.state.urls}/>
-      </main>
-    );
-  }
+	componentDidMount() {
+		fetchData
+			.getUrls()
+			.then((data) => this.setState({ urls: data.urls }))
+			.catch((error) => this.setState({ error: error }))
+	}
+	addUrl = (newUrl) => {
+		fetchData
+			.postUrl(newUrl)
+			.then((response) => {
+				if (!response.ok) {
+					this.setState({ error: 'Something went wrong... try again later' })
+				} else {
+					response.json()
+				}
+			})
+			.then(() => {
+				fetchData
+					.getUrls()
+					.then((data) => this.setState({ urls: data.urls }))
+					.catch((error) => this.setState({ error: error }))
+			})
+	}
+	render() {
+		return (
+			<main className='App'>
+				<header>
+					<h1>URL Shortener</h1>
+					<UrlForm addUrl={this.addUrl} />
+				</header>
+				{this.state.urls && <UrlContainer urls={this.state.urls} />}
+			</main>
+		)
+	}
 }
 
-export default App;
+export default App
